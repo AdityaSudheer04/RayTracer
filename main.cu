96ced10d119438e8ed33a0caba3dd9ca0cdbf595
Zarvenik <adityasudheer.221ee102@nitk.edu.in>
2024-05-10 21:21:17 +0530

    Add project files.

---
+++ main.cu
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <time.h>
+
+// limited version of checkCudaErrors from helper_cuda.h in CUDA examples
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+void check_cuda(cudaError_t result, char const* const func, const char* const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
+
+__global__ void render(float* fb, int max_x, int max_y) {
+    
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if ((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j * max_x * 3 + i * 3;
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.2;
+}
+
+int main() {
+    int image_width = 1200;
+    int image_height = 600;
+    int tx = 8;
+    int ty = 8;
+
+    std::cerr << "Rendering a " << image_width << "x" << image_height << " image ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+    int num_pixels = image_width * image_height;
+    size_t fb_size = 3 * num_pixels * sizeof(float);
+
+    // allocate Frame Buffer
+    // Frame Buffer is a float array that contains all the values of RGB fo each pixel
+    // from top left to bottom right
+    float* fb;
+    checkCudaErrors(cudaMallocManaged((void**)&fb, fb_size));
+
+    clock_t start, stop;
+    start = clock();
+
+    // Render our buffer
+    dim3 blocks(image_width / tx + 1, image_height / ty + 1);
+    dim3 threads(tx, ty);
+    render << <blocks, threads >> > (fb, image_width, image_height);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+    stop = clock();
+    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    std::cerr << "took " << timer_seconds << " seconds.\n";
+
+    // Output FB as Image
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    for (int j = image_height - 1; j >= 0; j--) {
+        for (int i = 0; i < image_width; i++) {
+            size_t pixel_index = j * 3 * image_width + i * 3;
+            float r = fb[pixel_index + 0];
+            float g = fb[pixel_index + 1];
+            float b = fb[pixel_index + 2];
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    checkCudaErrors(cudaFree(fb));
+}
